74315e5fbec452489bc5e2a71009b5773e3e9457
Alice Chen <chiachun78@gmail.com>
2014-05-26 16:02:55 -0700

    Worked on counter

--- app/count.js
+++ app/count.js
@@ -1,8 +1,32 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
+   /**
+   * This function takes two arguments: a starting number,
+   * and an ending number. The function should console.log each number from the start
+   * number to the end number, one number per 1/10th of a second. The function should
+   * return an object with a cancel method, which should cancel the counting.
+   */
   return {
-    count : function (start, end) {
-    }
+  	count : function (start, end) {
+  		var timeoutID;
+
+  		// note: if we do this with a loop and a closure for each index, 
+  		// the numbers will not be logged every 0.1 second because setTimeout is asychonous!
+  		var count = function() {
+  			console.log(start++);
+  			if (start <= end) {
+  				timeoutID = setTimeout(count, 100);	
+  			}
+  		}
+
+  		count();
+
+  		return {
+  			cancel: function() {
+  				timeoutID && clearTimeout(timeoutID);
+  			}
+  		}
+  	}
   };
 });
