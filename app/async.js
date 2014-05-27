49c85d70c463ccd2fb0f8f8369d2203e07f8569e
Alice Chen <chiachun78@gmail.com>
2014-05-26 19:27:13 -0700

    Worked on async

--- app/async.js
+++ app/async.js
@@ -3,11 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+    	return new Promise(function(resolve, reject) {
+    		resolve(value);
+    	});
     },
 
     manipulateRemoteData : function(url) {
+    	var deferred = $.Deferred();
 
+    	$.get(url).then(function(data) {
+    		var names = $.map(data.people, function(person) {
+    			return person.name;
+    		});
+    		deferred.resolve(names.sort());
+    	});
+
+    	return deferred.promise();
     }
   };
 });
