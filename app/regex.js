25f66ded8f407d1d3a88ace0e9858666fde14c82
Alice Chen <chiachun78@gmail.com>
2014-05-28 21:34:29 -0700

    Worked on regex

--- app/regex.js
+++ app/regex.js
@@ -3,25 +3,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return /[0-9]/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        return /([a-z])\1+/i.test(str); // \1 back references to the matched character
     },
 
     endsWithVowel : function(str) {
+        return /[aeiou]$/i.test(str);
 
     },
 
     captureThreeNumbers : function(str) {
-
+        var match = str.match(/[0-9]{3}/);
+        return match ? match[0] : false;
     },
 
     matchesPattern : function(str) {
-
+        return /^\d{3}-\d{3}-\d{4}$/.test(str);
     },
+
     isUSD : function(str) {
+        return /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/.test(str)
 
     }
   };
